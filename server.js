c64227358de1425953a3a3b0a392da4001f3c97f
Tony Herrera <tonyeherrera@github.com>
2022-11-01 22:20:51 -0400

    server adjustments

--- server.js
+++ server.js
@@ -4,18 +4,23 @@ require("dotenv").config()
 const morgan = require("morgan")
 const mongoose = require("mongoose")
 const {expressjwt} = require('express-jwt')
+const path = require("path")
+const process = require("process");
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({path: __dirname+'/.env'});
+}
 
-
+process.env.SECRET
 
 app.use(express.json())
 app.use(morgan("dev"))
 
 mongoose.connect(process.env.MONGOURI, ()=> console.log("Connected to DB"))
 
-app.use('/auth', require('./routes/authRouter.js'))
+app.use('/auth', require(path.join(__dirname,'routes','authRouter')))
 app.use('/api', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256']}))
-app.use('/api/userDrinks', require('./routes/userDrinksRouter.js'))
+app.use('/api/userDrinks', require(path.join(__dirname,'routes','userDrinksRouter')))
 
 
 app.use((err, req, res, next) => {
@@ -31,3 +36,11 @@ const port = process.env.PORT || 9000
 app.listen(port, () => {
     console.log('Server is running on local port 9000')
 })
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, './client', 'build')));
+    app.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname, './client', 'build', 'index.html'));
+    })
+  }
+  
