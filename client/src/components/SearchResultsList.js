8628a112eec7804e72a98170a5613f2228402ed9
tonyeherrera <tony.e.herrera@gmail.com>
2023-05-22 00:42:52 -0400

    search bug

--- client/src/components/SearchResultsList.js
+++ client/src/components/SearchResultsList.js
@@ -8,7 +8,7 @@ function SearchResultsList(){
     
     return(
         <div className="search--list">
-            {searchResults.length < 0  ? <h1 style={{justifySelf:"center"}}>Your search results will display here!</h1> : searchResults.map(drink => <RecipeCard {...drink} save={saveRecipe} key={drink.idDrink} path="search"/>)}
+            {searchResults && searchResults.length > 0  ? searchResults.map(drink => <RecipeCard {...drink} save={saveRecipe} key={drink.idDrink} path="search"/>) : !searchResults ?  <h1 style={{justifySelf:"center"}}>Sorry, no results found for that entry</h1> : <h1>Your search results will display here!</h1> }
         </div>
     )
 } 
